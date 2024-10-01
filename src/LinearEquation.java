abc41eee5f5bbd77eb21e5060ab932b2f64ed187
BT_2S14_20 <@email>
2024-10-01 11:05:13 -0400

    Added LinearEquation and EquationRunner classes

---
+++ src/LinearEquation.java
@@ -0,0 +1,17 @@
+public class LinearEquation {
+    private int x;
+    private int y;
+    private int x2;
+    private int y2;
+    private double newX;
+
+    public LinearEquation(int x, int y, int x2, int y2){
+        this.x=x;
+        this.y=y;
+        this.x2=x2;
+        this.y2=y2;
+    }
+    public double slope(){
+        return
+    }
+}
