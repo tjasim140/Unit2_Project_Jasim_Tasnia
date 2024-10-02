669aa50c5c6f1b1ea8dd15b705ae9a366ab37072
BT_2S14_20 <@email>
2024-10-02 10:57:09 -0400

    Added user input to take coordinates to find slope between two points and parse integers

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -4,11 +4,17 @@ public class EquationRunner {
         Scanner s = new Scanner(System.in);
         System.out.print("Type first coordinate: ");
         String x = s.nextLine();
-        int xx = Integer.parseInt(x);
-        System.out.println("Type second coord: ");
+        String x1 = x.substring(1,2); // Fix to include multiple digit numbers
+        int xx1 = Integer.parseInt(x1);
+        String y1 = x.substring(3,4);
+        int yy1 = Integer.parseInt(y1);
+        System.out.println("Type second coordinate: ");
         String y = s.nextLine();
-        int yy = Integer.parseInt(y);
-        LinearEquation coords = new LinearEquation(xx,yy,10,20);
+        String x2 = y.substring(1,2);
+        int xx2 = Integer.parseInt(x2);
+        String y2 = y.substring(3,4);
+        int yy2 = Integer.parseInt(y2);
+        LinearEquation coords = new LinearEquation(xx1,yy1,xx2,yy2);
         System.out.println(coords.slope());
     }
 }
