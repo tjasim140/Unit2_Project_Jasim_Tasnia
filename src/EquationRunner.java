c3f6565dcaa7b86f2becf2af374df9df04774554
BT_2S14_20 <@email>
2024-10-02 10:45:12 -0400

    Added slope method to LinearEquation class

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class EquationRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Type first coordinate: ");
+        String x = s.nextLine();
+        int xx = Integer.parseInt(x);
+        System.out.println("Type second coord: ");
+        String y = s.nextLine();
+        int yy = Integer.parseInt(y);
+        LinearEquation coords = new LinearEquation(xx,yy,10,20);
+        System.out.println(coords.slope());
+    }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -11,7 +11,29 @@ public class LinearEquation {
         this.x2=x2;
         this.y2=y2;
     }
-    public double slope(){
-        return
+    public String slope(){
+        if ((y2-y) % (x2-x) == 0) {
+            if ((((y2-y)/(x2-x))==1)) {
+                return "";
+            }else {
+                int result = ((y2 - y) / (x2 - x));
+                    return "" + result;
+            }
+        } else if ((((y2-y)/(x2-x)) == -1)) {
+            return "-";
+        } else if (((y2-y) < 1)) {
+            if ((x2-x) < 1) {
+                return Math.abs(y2 - y) + "/" + Math.abs(x2 - x);
+            }
+            else {
+                return "-" + Math.abs(y2-y) + "/" + (x2-x);
+            }
+        }
+        else if ((x2-x) < 1) {
+            return (y2-y) + "/" + Math.abs(x2-x);
+        }
+        else {
+            return (y2-y) + "/" + (x2-x);
+        }
     }
 }
