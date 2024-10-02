9f297e3a5299cf0ed948165bf26fcb16803debe9
BT_2S14_20 <@email>
2024-10-02 11:03:55 -0400

    Fixed coordinate scanner to take numbers of any number of digits using indexOf()

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -4,9 +4,9 @@ public class EquationRunner {
         Scanner s = new Scanner(System.in);
         System.out.print("Type first coordinate: ");
         String x = s.nextLine();
-        String x1 = x.substring(1,2); // Fix to include multiple digit numbers
+        String x1 = x.substring(1,x.indexOf(",")); // Fix to include multiple digit numbers, use index()
         int xx1 = Integer.parseInt(x1);
-        String y1 = x.substring(3,4);
+        String y1 = x.substring(x.indexOf(",")+1,x.indexOf(")"));
         int yy1 = Integer.parseInt(y1);
         System.out.println("Type second coordinate: ");
         String y = s.nextLine();
