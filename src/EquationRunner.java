11ce521ea06ce80ae2d2034ed3438ecb8e213e47
Tasnia <@gmail.com>
2024-10-14 16:08:52 -0400

    Added comments to explain code and changed method newX so it takes new x-value as an input and the new coordinate is printed seprately after toString() method is called on object coords

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -2,22 +2,33 @@ import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        //Takes user input of a point in (x,y) format then parses out x and y as integers
         System.out.print("Type first coordinate: ");
-        String x = s.nextLine();
-        String x1 = x.substring(1, x.indexOf(","));
+        String a = s.nextLine();
+        String x1 = a.substring(1, a.indexOf(",")); //substring of x value
         int xx1 = Integer.parseInt(x1);
-        String y1 = x.substring(x.indexOf(",")+1,x.indexOf(")"));
+        String y1 = a.substring(a.indexOf(",")+1,a.indexOf(")"));//substring of y value
         int yy1 = Integer.parseInt(y1);
+
+        //Takes user input of a second point in (x,y) format then parses out x2 and y2 as integers
         System.out.println("Type second coordinate: ");
-        String y = s.nextLine();
-        String x2 = y.substring(1, y.indexOf(","));
+        String b = s.nextLine();
+        String x2 = b.substring(1, b.indexOf(","));//substring of x2 value
         int xx2 = Integer.parseInt(x2);
-        String y2 = y.substring(y.indexOf(",")+1,y.indexOf(")"));
+        String y2 = b.substring(b.indexOf(",")+1,b.indexOf(")"));//substring of y2 value
         int yy2 = Integer.parseInt(y2);
+
+        //Creates new LinearEquation object using user input x and y values as attributes
+        LinearEquation coords = new LinearEquation(xx1,yy1,xx2,yy2);
+
+        //Prints information about the two points and the line they make using toString method
+        System.out.println(coords);
+
+        //Asks for user input for a new x-value
         System.out.println("Type new x-coordinate value: ");
         String nX = s.nextLine();
-        double newX = Double.parseDouble(nX);
-        LinearEquation coords = new LinearEquation(xx1,yy1,xx2,yy2,newX);
-        System.out.println(coords);
+        double newX = Double.parseDouble(nX); //converts x-value to a double value from a String
+        System.out.println(coords.newX(newX)); //prints result of newX() method, print new coordinate point that falls on the found line that includes the given x-value
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,18 +3,19 @@ public class LinearEquation {
     private int y;
     private int x2;
     private int y2;
-    private double newX;
 
-    public LinearEquation(int x, int y, int x2, int y2,double newX){
+    //Constructor that creates a new Linear Equation object with x,x2,y, and y2 values
+    public LinearEquation(int x, int y, int x2, int y2){
         this.x=x;
         this.y=y;
         this.x2=x2;
         this.y2=y2;
-        this.newX=newX;
     }
+    //Calculates and formats slope based on coordinate points, can be negative/positive, integer/fraction slopes
     public String slope() {
-        //if whole # slope
+        //if whole # slope returns slope as an integer
         if ((y2 - y) % (x2 - x) == 0) {
+            //returns blank string so when the equation is printed, it prints x and not 1x
             if ((((y2 - y) / (x2 - x)) == 1)) {
                 return "";
             } else {
@@ -23,10 +24,14 @@ public class LinearEquation {
             }
         }
         else {
-            if ((((double)y2 - y) / (x2 - x) < 1)) {
+            //if slope is negative
+            if ((((double)y2 - y) / (x2 - x) < 0)) {
+                //formats slope so that slope is printed as -x not -1x if slope = -1
                 if (((((double)y2 - y) / (x2 - x)) == -1)) {
                     return "-";
-                } else if ((((double)x2 - x) < 1) && ((y2 - y) < 1)) {
+                }
+                //formats slope so that it is returned as -y/x and not y/-x if slope is negative and is a fraction
+                else if ((((double)x2 - x) < 1) && ((y2 - y) < 1)) {
                     return Math.abs(y2 - y) + "/" + Math.abs(x2 - x);
                 } else if (((double)x2 - x) < 1) {
                     return "-" + (y2 - y) + "/" + Math.abs(x2 - x);
@@ -39,10 +44,12 @@ public class LinearEquation {
         }
 
     }
+    //Returns distance between two given points using distance formula
     public String distance(){
         double d = Math.sqrt(Math.pow((x2-x),2)+Math.pow((y2-y),2));
         return String.format("%.2f",d);
     }
+    //returns y-int of the line
     public String yInt(){
         double yint;
         double s = ((double)(y2-y)/(x2-x));
@@ -50,30 +57,36 @@ public class LinearEquation {
         if (yint == 0){
             return "0";
             }
+        //rounds y-int to the nearest hundredth
         else{
             return String.format("%.2f",yint);
         }
     }
-    public String newX(){
+    //Finds point in line using given x-coord to solve for y, then returns point in format (x,y) rounded to the nearest hundredth as a printable string
+    public String newX(double nX){
         double yint=(y2-(((double)(y2-y)/(x2-x))*x2));
-        double newY = newX*((double)(y2-y)/(x2-x))+yint;
-        return "Point on Line: ("+newX+","+String.format("%.2f",newY)+")";
+        double newY = nX*((double)(y2-y)/(x2-x))+yint; //solves for y given x
+        return "Point on Line: ("+nX+","+String.format("%.2f",newY)+")";
     }
+    //Formats relevant information of the object as a printable string
     public String toString(){
         String s = slope();
         String yI = yInt();
         String str;
+        //leaves out + or minus sign so no y-int is printed when y-int = 0
         if (yI.contains("0")) {
             str = s + "x";
         }
+        //leaves out - sign if y-int is negative so +-y-int is not printed when formating equation
         else {
             if (yI.contains("-")) {
                 str = s + "x" + yI;
+        //Adds + so y-int is correctly printed in y=mx+b form
             } else {
                 str = s + "x+" + yI;
             }
         }
-        return "First Point: (" + x + ","+ y +")\nSecond Point: ("+x2+","+y2+")\nSlope: "+s+"\nY-intercept: "+yI+"\nDistance Between Points: "+distance()+"\n"+newX()+"\nEquation of Line: "+str;
+        return "First Point: (" + x + ","+ y +")\nSecond Point: ("+x2+","+y2+")\nSlope: "+s+"\nY-intercept: "+yI+"\nDistance Between Points: "+distance()+"\nEquation of Line: "+str;
     }
 
 }
